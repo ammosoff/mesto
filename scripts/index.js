c787184ed51a39e7ed7a5443602d25ca4a1a57c6
Nikolai Ammosoff <ammosoff@mail.ru>
2022-11-22 00:28:43 +0300

    feat: добавить обработку событий для попап на JavaScript

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,43 @@
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__button-close');
 const popupOpenButtonElement = document.querySelector('.profile__button-edit');
-/* const popupSaveButtonElement = popupElement.querySelector('.popup__button-save'); */
+const popupSubmitFormButtonElement = popupElement.querySelector('.popup__form');
+const popupFormInputNameElement = popupElement.querySelector('.popup__form-input_type_name');
+const popupFormInputCaptionElement = popupElement.querySelector('.popup__form-input_type_caption');
+const profileNameElement = document.querySelector('.profile__name');
+const profileCaptionElement = document.querySelector('.profile__caption');
 
-/* Создаем функции для открытия и закрытия попапа */
+/* openPopup - функция открытия попапа и получения текстовых значений элементов */
 
-const openPopup = function() { 
+function openPopup() { 
   popupElement.classList.add('popup_is-opened');
+  popupFormInputNameElement.value = profileNameElement.textContent;
+  popupFormInputCaptionElement.value = profileCaptionElement.textContent;
 }
 
-const closePopup = function() {
+/* closePopup - функция закрытия попапа */
+
+function closePopup() {
   popupElement.classList.remove('popup_is-opened');
 }
 
-/* Вызов функций по открытию и закрытию попапа */
+/* submitFormPopup - функция отправки формы без перезагрузки страницы
+так же функция позволяет изменить текстовое содержимое указанных элементов */
+
+function submitFormPopup(evt) {
+  evt.preventDefault();
+
+  profileNameElement.textContent = popupFormInputNameElement.value;
+  profileCaptionElement.textContent = popupFormInputCaptionElement.value;
+
+  closePopup();
+}
+
+/* Вызов функций */
 
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
+popupSubmitFormButtonElement.addEventListener('submit', submitFormPopup);
+
+
+
