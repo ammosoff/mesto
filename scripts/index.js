daea1c6818b1b0d51154054dd9357a6aeca02a64
Nikolai Ammosoff <ammosoff@mail.ru>
2022-12-04 15:11:26 +0300

    feat: добавить разметку и js для попапа с картинкой

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,21 @@
+/* элементы для добавления новой карточки */
+const cardsContainer = document.querySelector('.cards');
+const cardTemplate = document.querySelector('#card-template').content;
+
 /* попапы */
 const popupProfileEdit = document.querySelector('.popup_type_edit-profile');
 const popupCardAdd = document.querySelector('.popup_type_add-card');
+const popupViewPicture = document.querySelector('.popup_type_view-picture');
+const popupViewPictureImg = popupViewPicture.querySelector('.popup__img');
+const popupViewPictureCaption = popupViewPicture.querySelector('.popup__caption');
+
 const popupProfileEditOpenButton = document.querySelector('.profile__button-edit');
 const popupCardAddOpenButton = document.querySelector('.profile__button-add');
+/* const popupViewPictureOpenButton = cardTemplate.querySelector('.card__img'); */
+/* console.log(popupViewPictureOpenButton); */
 const popupProfileEditCloseButton = popupProfileEdit.querySelector('.popup__button-close');
 const popupCardAddCloseButton = popupCardAdd.querySelector('.popup__button-close');
+const popupViewPictureCloseButton = popupViewPicture.querySelector('.popup__button-close');
 /* данные из profile */
 const profileName = document.querySelector('.profile__name');
 const profileCaption = document.querySelector('.profile__caption');
@@ -15,9 +26,7 @@ const popupFormInputTitle = popupCardAdd.querySelector('.popup__form-input_type_
 const popupFormInputLink = popupCardAdd.querySelector('.popup__form-input_type_link')
 const popupProfileEditSubmitFormButton = popupProfileEdit.querySelector('.popup__form');
 const popupCardAddSubmitFormButton = popupCardAdd.querySelector('.popup__form');
-/* элементы для добавления новой карточки */
-const cardsContainer = document.querySelector('.cards');
-const cardTemplate = document.querySelector('#card-template').content;
+
 
 /* initialCards - массив объектов с информацией для добавления первых 6 карточек на странице  */
 
@@ -60,6 +69,15 @@ function closePopup(popup) {
   popup.classList.remove('popup_is-opened');
 }
 
+/* handleViewCard - функция просмотра изображения карточки */
+
+function handleViewCard(evt) {
+  popupViewPictureImg.src = evt.target.src;
+  popupViewPictureImg.alt = evt.target.closest('.card').querySelector('.card__caption').textContent;
+  popupViewPictureCaption.textContent = evt.target.closest('.card').querySelector('.card__caption').textContent;
+  openPopup(popupViewPicture);
+}
+
 /* handleLikeCard - функция лайка карточки */
 
 function handleLikeCard(evt) {
@@ -77,17 +95,19 @@ function handleDeleteCard(evt) {
 
 function addCard(dataCard) {
   const newCard = cardTemplate.cloneNode(true);
-
   const nameCard = newCard.querySelector('.card__caption');
   const imgCard = newCard.querySelector('.card__img');
   nameCard.textContent = dataCard.name;
   imgCard.src = dataCard.link;
+  imgCard.alt = dataCard.name;
 
   const cardleLikeButton = newCard.querySelector('.card__button-like');
   cardleLikeButton.addEventListener('click', handleLikeCard);
 
   const cardDeleteButton = newCard.querySelector('.card__button-delete');
   cardDeleteButton.addEventListener('click', handleDeleteCard);
+
+  imgCard.addEventListener('click', handleViewCard);
 
   return newCard;
 }
@@ -149,6 +169,14 @@ popupProfileEditCloseButton.addEventListener('click', function () {
 
 popupCardAddCloseButton.addEventListener('click', function () {
   closePopup(popupCardAdd);
+});
+
+popupViewPictureCloseButton.addEventListener('click', function () {
+  closePopup(popupViewPicture);
+});
+
+popupCardAddCloseButton.addEventListener('click', function () {
+  closePopup(popupViewPicture);
 });
 
 popupProfileEditSubmitFormButton.addEventListener('submit', submitFormPopupEditProfile);
