b13a2d6dcf001f5081cd78195ecb40ab7d410de8
Nikolai Ammosoff <ammosoff@mail.ru>
2022-12-15 15:06:58 +0300

    feat: сделать кнопку неактивной при невалидном заполнении инпутов

--- scripts/validate.js
+++ scripts/validate.js
@@ -23,18 +23,22 @@ const checkInputValidity = (formElement, inputElement) => {
   }
 };
 
-/* Принимает параметром элемент формы и добавляет ее полям нужные обработчики */
+/* Принимает параметром элемент формы и добавляет ее полям нужные обработчики, передает управление в checkInputValidity для проверки валидности полей форм */
 const setEventListeners = (formElement) => {
   const inputList = Array.from(formElement.querySelectorAll('.popup__form-input'));
+  const buttonElement = formElement.querySelector('.popup__button-save');
+
+  toggleButtonState(inputList, buttonElement);
 
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', () => {
       checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
     });
   });
 };
 
-/* Добавление обработчиков всем формам. Находим все формы в DOM и вызываем для них функцию setEventListeners */
+/* Находим все формы в DOM и вызываем для них функцию setEventListeners чтобы навесить обработчики инпутам */
 const enableValidation = () => {
   const formList = Array.from(document.querySelectorAll('.popup__form'));
 
@@ -45,6 +49,22 @@ const enableValidation = () => {
   
       setEventListeners(formElement);
   });
+}
+
+/* Принимает массив полей формы и возвращает true, если хотя бы одно из них не валидно, и false, если валидно */
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+} 
+
+/* отключает и включает кнопку отправки формы */
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add('popup__button-save_disabled');
+  } else {
+    buttonElement.classList.remove('popup__button-save_disabled');
+  }
 }
 
 enableValidation();
