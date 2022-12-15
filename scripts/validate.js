3b52cf2da7530be123425a4aacf7a6afa8a43e67
Nikolai Ammosoff <ammosoff@mail.ru>
2022-12-15 05:12:09 +0300

    feat: написать функции отображения и скрытия ошибки для input

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,21 +1,26 @@
 const formElement = document.querySelector('.popup__form');
 const formInput = formElement.querySelector('.popup__form-input');
+const formError = formElement.querySelector(`.${formInput.id}-error`);
 
 
 /* показывает элемент ошибки */
-const showInputError = (input) => {
+const showInputError = (input, errorMessage) => {
   input.classList.add('popup__form-input_type_error');
+  formError.textContent = errorMessage;
+  formError.classList.add('popup__form-input-error_active');
 };
 
 /* скрывает элемент ошибки */
 const hideInputError = (input) => {
   input.classList.remove('popup__form-input_type_error');
+  formError.classList.remove('popup__form-input-error_active');
+  formError.textContent = '';
 };
 
 /* проверяет валидность поля, внутри вызывает showInputError или hideInputError */
 const checkInputValidity = () => {
   if (!formInput.validity.valid) {
-    showInputError(formInput);
+    showInputError(formInput, formInput.validationMessage);
   } else {
     hideInputError(formInput);
   }
